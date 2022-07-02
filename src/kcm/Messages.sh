d5501b91bf3fa909fa79d3c92f79490a7d14e7fe
Volker Krause <vkrause@kde.org>
2022-07-02 16:16:41 +0200

    Add missing SPDX markers
    GIT_SILENT

--- src/kcm/Messages.sh
+++ src/kcm/Messages.sh
@@ -1,2 +1,5 @@
 #! /usr/bin/env bash
+# SPDX-FileCopyrightText: 2022 Volker Krause <vkrause@kde.org>
+# SPDX-License-Identifier: BSD-2-Clause
+
 $XGETTEXT `find . -name "*.cpp" -o -name "*.qml"` -o $podir/kcm_push_notifications.pot
