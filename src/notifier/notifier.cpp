95c7b766187004208f5ad2d7670530e04bc907c4
Volker Krause <vkrause@kde.org>
2025-04-03 19:20:35 +0200

    Add a push notification sender CLI tool
    This is useful for testing without having a real application server handy.
    This used to be doable with a curl POST, but with content encryption and
    VAPID that is no longer practical.
    
    We'll also need the same code for having a full round-trip self test in
    the KCM.

---
+++ src/notifier/notifier.cpp
@@ -0,0 +1,60 @@
+/*
+    SPDX-FileCopyrightText: 2025 Volker Krause <vkrause@kde.org>
+    SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include "notifier.h"
+
+#include "contentencryptor_p.h"
+#include "vapid_p.h"
+
+#include <QNetworkAccessManager>
+#include <QNetworkReply>
+#include <QNetworkRequest>
+
+using namespace KUnifiedPush;
+
+Notifier::Notifier(QObject *parent)
+    : QObject(parent)
+{
+}
+
+Notifier::~Notifier() = default;
+
+void Notifier::submit(const QByteArray &msg, QNetworkAccessManager *nam)
+{
+    assert(nam);
+
+    QNetworkRequest req(m_endpoint);
+    req.setRawHeader("TTL", QByteArray::number(m_ttl.count()));
+
+    QByteArray content;
+    if (!m_userAgentPublicKey.isEmpty() && !m_authSecret.isEmpty()) {
+        ContentEncryptor encryptor(m_userAgentPublicKey, m_authSecret);
+        content = encryptor.encrypt(msg);
+        req.setRawHeader("Content-Encoding", "aes128gcm");
+    } else {
+        content = msg;
+    }
+
+    if (!m_vapidPublicKey.isEmpty() && !m_vapidPrivateKey.isEmpty()) {
+        Vapid vapid(m_vapidPublicKey, m_vapidPrivateKey);
+        vapid.setContact(m_contact);
+        req.setRawHeader("Authorization", vapid.authorization(m_endpoint));
+    }
+
+#if 0
+    for (const auto &hdr : req.rawHeaderList()) {
+        qDebug() << hdr << req.rawHeader(hdr);
+    }
+#endif
+
+    auto reply = nam->post(req, content);
+    reply->setParent(this);
+    connect(reply, &QNetworkReply::finished, this, [this, reply]() {
+        reply->deleteLater();
+        Q_EMIT finished(reply);
+    });
+}
+
+#include "moc_notifier.cpp"
---
+++ src/notifier/notifier.h
@@ -0,0 +1,57 @@
+/*
+    SPDX-FileCopyrightText: 2025 Volker Krause <vkrause@kde.org>
+    SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#ifndef KUNIFIEDPUSH_NOTIFIER_NOTIFIER_H
+#define KUNIFIEDPUSH_NOTIFIER_NOTIFIER_H
+
+#include <QObject>
+#include <QUrl>
+
+class QNetworkAccessManager;
+class QNetworkReply;
+
+namespace KUnifiedPush {
+
+/** Push notification submission job.
+ *  This is only used for testing/tooling.
+ */
+class Notifier : public QObject
+{
+    Q_OBJECT
+public:
+    explicit Notifier(QObject *parent = nullptr);
+    ~Notifier();
+
+    inline void setEndpoint(const QUrl &url) { m_endpoint = url; }
+    inline void setTtl(std::chrono::seconds ttl) { m_ttl = ttl; }
+    inline void setUserAgentPublicKey(const QByteArray &uaPublicKey) { m_userAgentPublicKey = uaPublicKey; }
+    inline void setAuthSecret(const QByteArray &authSecret) { m_authSecret = authSecret; }
+    inline void setContact(const QString &contact) { m_contact = contact; }
+    inline void setVapidPublicKey(const QByteArray &vapidPublicKey) { m_vapidPublicKey = vapidPublicKey; }
+    inline void setVapidPrivateKey(const QByteArray &vapidPrivateKey) { m_vapidPrivateKey = vapidPrivateKey; }
+
+    /** Submit push message with content @p msg. */
+    void submit(const QByteArray &msg, QNetworkAccessManager *nam);
+
+Q_SIGNALS:
+    void finished(QNetworkReply *reply);
+
+private:
+    QUrl m_endpoint;
+    std::chrono::seconds m_ttl = std::chrono::hours(1);
+
+    // content encryption
+    QByteArray m_userAgentPublicKey;
+    QByteArray m_authSecret;
+
+    // VAPID
+    QString m_contact;
+    QByteArray m_vapidPublicKey;
+    QByteArray m_vapidPrivateKey;
+};
+
+}
+
+#endif
