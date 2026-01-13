0d1fc24acbe2d8d09cf15e9598bf327f2c190951
Nicolas Fella <nicolas.fella@gmx.de>
2026-01-13 20:50:01 +0100

    Set up KAboutData

--- src/distributor/main.cpp
+++ src/distributor/main.cpp
@@ -7,18 +7,25 @@
 #include "logging.h"
 
 #include "../shared/unifiedpush-constants.h"
+#include "kunifiedpush_version.h"
 
 #include <QCoreApplication>
 #include <QDBusConnection>
 
+#include <KAboutData>
+
 using namespace KUnifiedPush;
+using namespace Qt::StringLiterals;
 
 int main(int argc, char **argv)
 {
-    QCoreApplication::setOrganizationDomain(QStringLiteral("kde.org"));
     QCoreApplication::setOrganizationName(QStringLiteral("KDE"));
 
     QCoreApplication app(argc, argv);
+
+    KAboutData about(u"kunifiedpush-distributor"_s, QString(), QString::fromLatin1(KUNIFIEDPUSH_VERSION_STRING));
+    KAboutData::setApplicationData(about);
+
     KUnifiedPush::Distributor distributor;
     if (!QDBusConnection::sessionBus().registerService(KDE_DISTRIBUTOR_SERVICE_NAME)) {
         qCCritical(Log) << "Distributor service name already in use - aborting!";
