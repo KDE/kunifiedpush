ef85aab51cbb8bfdedc2eb20d514fdcaaf12d7af
Thiago Sueto <herzenschein@gmail.com>
2026-01-14 21:50:45 -0300

    QDoc NextcloudAuthenticator

--- src/kcm/nextcloudauthenticator.h
+++ src/kcm/nextcloudauthenticator.h
@@ -13,15 +13,28 @@
 class QNetworkAccessManager;
 class QNetworkReply;
 
-/** Implementation of https://docs.nextcloud.com/server/latest/developer_manual/client_apis/LoginFlow/index.html#login-flow-v2. */
+/*!
+ * \class NextcloudAuthenticator
+ * \inmodule KUnifiedPush
+ * Implementation of Nextcloud's LoginFlow v2.
+ * \sa https://docs.nextcloud.com/server/latest/developer_manual/client_apis/LoginFlow/index.html#login-flow-v2
+ */
 class NextcloudAuthenticator : public QObject
 {
     Q_OBJECT
 public:
+    /*!
+     */
     explicit NextcloudAuthenticator(QObject *parent = nullptr);
+    /*!
+     */
     ~NextcloudAuthenticator();
 
+    /*!
+     */
     void setNetworkAccessManager(QNetworkAccessManager *nam);
+    /*!
+     */
     void authenticate(const QUrl &baseUrl, const QString &appName);
 
 Q_SIGNALS:
